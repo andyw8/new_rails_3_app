149a120d5a4732b60897b71456cbf16427600494
andyw8 <andy@andywaite.com>
2010-10-23 11:47:14 +0100

    New Rails app
    rails new certfolio --skip-test-unit --skip-prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Certfolio::Application.config.secret_token = '84e3d310cdec85e78d4255cd79a3fd4ec2b9128f81f46cbff076c2423265e45b6deb292089896c2781142c4bc51eeac7e7ae573ad32fb7aef11020dafe98166e'
