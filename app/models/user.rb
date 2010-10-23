6f2201acf47e4a6e3c57cbf70ed9dc45392716b7
andyw8 <andy@andywaite.com>
2010-10-23 23:09:46 +0100

    Tweaks to Create Users migration for Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   
   devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable, :registerable
   
--- db/migrate/20101023195449_create_users.rb
+++ db/migrate/20101023195449_create_users.rb
@@ -2,13 +2,17 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :email
-      t.database_authenticatable
+      t.database_authenticatable :null => false
       t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
       t.timestamps
     end
+
+    add_index :users, :email,                :unique => true
+    add_index :users, :reset_password_token, :unique => true
+
   end
 
   def self.down
