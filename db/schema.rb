f9253e7c15023e6965e8d1bdc36013a0cf42275b
andyw8 <andy@andywaite.com>
2010-10-31 17:02:03 +0000

    Adding assets table (for paperclip)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101023195449) do
+ActiveRecord::Schema.define(:version => 20101031165751) do
+
+  create_table "assets", :force => true do |t|
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.integer  "integer"
+    t.datetime "attachment_updated_at"
+    t.integer  "user_id"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -55,8 +67,5 @@ ActiveRecord::Schema.define(:version => 20101023195449) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
